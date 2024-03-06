bcf9c537806ca0421ef0db8b3f094ecf0d00c567
yamatatsu <the.kinnikuman@gmail.com>
2024-03-06 19:25:56 +0900

    experiment

--- packages/dynim/tests/globalSetup.ts
+++ packages/dynim/tests/globalSetup.ts
@@ -22,59 +22,7 @@ export const setup = async () => {
 	}
 
 	await createTable(TableNames, {
-		TableName: "test-PREFIX-Device",
-		KeySchema: [{ AttributeName: "yanecubeId", KeyType: "HASH" }],
-		AttributeDefinitions: [
-			{ AttributeName: "yanecubeId", AttributeType: "S" },
-			{ AttributeName: "institutionId", AttributeType: "S" },
-		],
-		GlobalSecondaryIndexes: [
-			{
-				IndexName: "institutionIdIndex",
-				KeySchema: [{ AttributeName: "institutionId", KeyType: "HASH" }],
-				Projection: { ProjectionType: "ALL" },
-				ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
-			},
-		],
-		ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
-	});
-
-	await createTable(TableNames, {
-		TableName: "test-PREFIX-DeviceHistory",
-		KeySchema: [
-			{ AttributeName: "yanecubeId", KeyType: "HASH" },
-			{ AttributeName: "timestamp", KeyType: "RANGE" },
-		],
-		AttributeDefinitions: [
-			{ AttributeName: "yanecubeId", AttributeType: "S" },
-			{ AttributeName: "timestamp", AttributeType: "S" },
-		],
-		ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
-	});
-
-	await createTable(TableNames, {
-		TableName: "test-PREFIX-InstitutionAvailability",
-		KeySchema: [
-			{ AttributeName: "institutionId", KeyType: "HASH" },
-			{ AttributeName: "date", KeyType: "RANGE" },
-		],
-		AttributeDefinitions: [
-			{ AttributeName: "institutionId", AttributeType: "S" },
-			{ AttributeName: "date", AttributeType: "S" },
-		],
-		GlobalSecondaryIndexes: [
-			{
-				IndexName: "dateIndex",
-				KeySchema: [{ AttributeName: "date", KeyType: "HASH" }],
-				Projection: { ProjectionType: "ALL" },
-				ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
-			},
-		],
-		ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
-	});
-
-	await createTable(TableNames, {
-		TableName: "test-PREFIX-MarketPrice",
+		TableName: "dynim-test-pk-S-sk-S",
 		KeySchema: [
 			{ AttributeName: "pk", KeyType: "HASH" },
 			{ AttributeName: "sk", KeyType: "RANGE" },
--- packages/dynim/tests/main.test.ts
+++ packages/dynim/tests/main.test.ts
@@ -1,38 +1,121 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
+// import { DeserializeMiddleware, MetadataBearer } from "@smithy/types";
 import { expect, test } from "vitest";
 
-const client = new DynamoDBClient(
-	process.env.VITEST
-		? {
-				endpoint: "http://127.0.0.1:8000",
-				region: "local",
-				credentials: {
-					accessKeyId: "fakeMyKeyId",
-					secretAccessKey: "fakeSecretAccessKey",
-				},
-		  }
-		: {},
-);
-const doc = DynamoDBDocument.from(client);
-client.middlewareStack.add(
-	() => async (args) => {
-		// const res = await next(args);
-		console.log({ args });
-		return {
-			response: new Response(),
-			output: { $metadata: { requestId: "test-requestId" } },
-		};
+// // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+// const store = new Map<string, any>();
+// const middleware: DeserializeMiddleware<object, MetadataBearer> =
+// 	(next) => async (args) => {
+// 		const res = await next(args);
+// 		console.log({ args: JSON.stringify(args) });
+// 		console.log({
+// 			output: JSON.stringify(res.output),
+// 		});
+
+// 		let command: string | undefined;
+// 		if (
+// 			args.request instanceof Object &&
+// 			"headers" in args.request &&
+// 			args.request.headers instanceof Object &&
+// 			"x-amz-target" in args.request.headers &&
+// 			typeof args.request.headers["x-amz-target"] === "string"
+// 		) {
+// 			command = args.request.headers["x-amz-target"].split(".")[1];
+// 		}
+
+// 		if (command === "PutItem") {
+// 			store.set(args.input.TableName, args.input.Item);
+// 			return {
+// 				response: new Response(),
+// 				output: {
+// 					$metadata: {
+// 						attempts: 1,
+// 						cfId: undefined,
+// 						extendedRequestId: undefined,
+// 						httpStatusCode: 200,
+// 						requestId: "8c2cbda9-e05f-4b11-ad7e-c10e876a2f5a",
+// 						totalRetryDelay: 0,
+// 					},
+// 				},
+// 			};
+// 		} else if (command === "GetItem") {
+// 			const item = store.get(args.input.TableName);
+// 			if (item) {
+// 				return {
+// 					response: new Response(),
+// 					output: {
+// 						$metadata: {
+// 							attempts: 1,
+// 							cfId: undefined,
+// 							extendedRequestId: undefined,
+// 							httpStatusCode: 200,
+// 							requestId: "8c2cbda9-e05f-4b11-ad7e-c10e876a2f5a",
+// 							totalRetryDelay: 0,
+// 						},
+// 						Item: item,
+// 					},
+// 				};
+// 			}
+// 		}
+
+// 	throw new Error(`unexpected command: ${command}`);
+
+// 	// return {
+// 	// 	response: new Response(),
+// 	// 	output: {
+// 	// 		$metadata: {
+// 	// 			attempts: 1,
+// 	// 			cfId: undefined,
+// 	// 			extendedRequestId: undefined,
+// 	// 			httpStatusCode: 200,
+// 	// 			requestId: "8c2cbda9-e05f-4b11-ad7e-c10e876a2f5a",
+// 	// 			totalRetryDelay: 0,
+// 	// 		},
+// 	// 		Item: {
+// 	// 			message: "hello",
+// 	// 			pk: "foo",
+// 	// 			sk: "bar",
+// 	// 		},
+// 	// 	},
+// 	// };
+// };
+
+const client = new DynamoDBClient({
+	endpoint: "http://127.0.0.1:8000",
+	region: "local",
+	credentials: {
+		accessKeyId: "fakeMyKeyId",
+		secretAccessKey: "fakeSecretAccessKey",
 	},
-	{ step: "initialize" },
-);
-// doc.middlewareStack.add((next) => async (args) => {})
+});
+// client.middlewareStack.add(middleware, { step: "build" });
+const doc = DynamoDBDocument.from(client);
 
 test("should pass", async () => {
-	const res = await doc.get({
-		TableName: "test-PREFIX-Device",
-		Key: { yanecubeId: "aaa" },
+	await doc.put({
+		TableName: "dynim-test-pk-S-sk-S",
+		Item: { pk: "foo", sk: "bar", message: "hello" },
 	});
 
-	expect(res).toEqual({ $metadata: { requestId: "test-requestId" } });
+	const res = await doc.get({
+		TableName: "dynim-test-pk-S-sk-S",
+		Key: { pk: "foo", sk: "bar" },
+	});
+
+	expect(res).toEqual({
+		$metadata: {
+			attempts: 1,
+			cfId: undefined,
+			extendedRequestId: undefined,
+			httpStatusCode: 200,
+			requestId: expect.stringMatching(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/),
+			totalRetryDelay: 0,
+		},
+		Item: {
+			message: "hello",
+			pk: "foo",
+			sk: "bar",
+		},
+	});
 });
